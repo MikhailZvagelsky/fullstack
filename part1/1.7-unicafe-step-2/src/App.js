1e96a4d743149ed01e8cbb96f110fa6e2a86cab6
Mikhail Zvagelsky <mikhailzvagelsky@gmail.com>
2023-01-06 17:32:55 +0200

    Implement exercise 1.7.

---
+++ part1/1.7-unicafe-step-2/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const count = good + neutral + bad;
+
+  function createClickHandler(count, setCount) {
+    return () => setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>give feedback</h1>
+      <button onClick={createClickHandler(good, setGood)}>good</button>
+      <button onClick={createClickHandler(neutral, setNeutral)}>neutral</button>
+      <button onClick={createClickHandler(bad, setBad)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {count}</p>
+      <p>average {(good - bad)/count}</p>
+      <p>positive {(good * 100) / count} %</p>
+    </div>
+  );
+}
+
+export default App;
