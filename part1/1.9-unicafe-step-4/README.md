6448329e04f4d44f569f892bf885489558300f84
Mikhail Zvagelsky <mikhailzvagelsky@gmail.com>
2023-01-06 18:10:30 +0200

    Implement exercise 1.9

---
+++ part1/1.9-unicafe-step-4/README.md
@@ -0,0 +1,5 @@
+# 1.8: unicafe step4
+
+Change your application to display statistics only once feedback has been gathered.
+
+![App view](./images/app-view.png)
---
+++ part1/1.9-unicafe-step-4/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+const Statistics = ({good, neutral, bad}) => {
+  const count = good + neutral + bad;
+  if (count === 0) {
+    return (
+      <>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </>
+    );
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {count}</p>
+      <p>average {(good - bad)/count}</p>
+      <p>positive {(good * 100) / count} %</p>
+    </>
+  );
+}
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  function createHandler(count, setCount) {
+    return () => setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>give feedback</h1>
+      <button onClick={createHandler(good, setGood)}>good</button>
+      <button onClick={createHandler(neutral, setNeutral)}>neutral</button>
+      <button onClick={createHandler(bad, setBad)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+export default App;
