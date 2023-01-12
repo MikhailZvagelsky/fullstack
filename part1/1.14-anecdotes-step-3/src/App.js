08b06ba697e3bd7cfee53dc94785acec8feaaad5
Mikhail Zvagelsky <mikhailzvagelsky@gmail.com>
2023-01-12 22:04:07 +0200

    Implement exercise 1.14.

---
+++ part1/1.14-anecdotes-step-3/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ];
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const initialScores = Array(anecdotes.length).fill(0);
+  const [scores, setScores] = useState(initialScores);
+
+  const upvote = () => {
+    const newScores = [...scores];
+    newScores[selectedIndex] += 1;
+    setScores(newScores);
+  };
+
+  const selectNewAnecdote = () => {
+    const randomAnecdoteIndex = getRandomInt(0, anecdotes.length);
+    setSelectedIndex(randomAnecdoteIndex);
+  };
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const getMostPopularAnecdote = () => {
+    const maxScore = Math.max(...scores);
+    const maxScoreIndex = scores.indexOf(maxScore);
+    return anecdotes[maxScoreIndex];
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selectedIndex]}</div>
+      <button onClick={upvote}>vote</button>
+      <button onClick={selectNewAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{getMostPopularAnecdote()}</p>
+    </div>
+  )
+}
+
+export default App;
